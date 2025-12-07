06e0b79477e28a0cb0add76a09ac94c5d46cb1e5
dayne <dayne@scoir.com>
2025-12-07 23:28:33 +0000

    another attempt without strings

--- 2025/day7.2/main.go
+++ 2025/day7.2/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"os"
-	"strings"
 )
 
 type BeamRun struct {
-	Grid []string
-	Row  int
-	Col  int
+	Row           int
+	Col           int
+	TimeLineCount int
+}
+
+func (b BeamRun) Key() string {
+	return fmt.Sprintf("(%d,%d)", b.Row, b.Col)
 }
 
 func main() {
@@ -23,72 +25,112 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	var rows []string
+	rowIdx := 0
+	run := &BeamRun{
+		TimeLineCount: 1,
+	}
+	splitters := make(map[string]bool)
+	width := 0
 	for scanner.Scan() {
-		row := scanner.Text()
-		rows = append(rows, row)
+		row := scanner.Bytes()
+		if rowIdx == 0 {
+			width = len(row)
+		}
+		for colIdx, char := range row {
+			if char == '^' {
+				splitters[fmt.Sprintf("%d,%d", rowIdx, colIdx)] = true
+			}
+			if rowIdx == 0 && char == 'S' {
+				run.Col = colIdx
+			}
+		}
+		rowIdx++
 	}
+	height := rowIdx
 
-	rows[0] = strings.ReplaceAll(rows[0], "S", "|")
+	referenceSplits := map[string]*BeamRun{}
 
-	queue := []BeamRun{
-		{Grid: rows, Row: 0, Col: strings.Index(rows[0], "|")},
-	}
+	// fmt.Printf("%+v\n", run)
 
+	queue := []*BeamRun{run}
 	timelines := 1
-
-	maxIterations := 0
+	iterations := 0
 	for len(queue) > 0 {
-		run := queue[0]
+		current := queue[0]
+		queue = queue[1:]
 
-		if maxIterations > 5000000 {
-			println("Max iterations reached, stopping to prevent infinite loop.")
+		iterations++
+		if iterations > 10000000000 {
+			fmt.Println("exhausted iterations")
 			break
 		}
-		maxIterations++
+		// printGrid(splitters, height, width, current)
+		// fmt.Println("----")
 
-		nextRow := run.Row + 1
-		if nextRow >= len(run.Grid) {
+		nextRow := current.Row + 1
+		if nextRow >= height {
 			continue
 		}
-		nextChar := run.Grid[nextRow][run.Col]
-		if nextChar == '.' {
-			run.Grid[nextRow] = run.Grid[nextRow][:run.Col] + "|" + run.Grid[nextRow][run.Col+1:]
-			run.Row = nextRow
-			queue[0] = run
-			continue
-		}
-		queue = queue[1:]
-		if nextChar == '^' {
-			split := false
-			if run.Col > 0 {
-				grid := append([]string{}, run.Grid...)
-				row := bytes.Clone([]byte(grid[nextRow]))
-				row[run.Col-1] = '|'
-				grid[nextRow] = string(row)
-				nextRun := BeamRun{Grid: grid, Row: nextRow, Col: run.Col - 1}
-				queue = append(queue, nextRun)
-				split = true
+		nextPosition := fmt.Sprintf("%d,%d", nextRow, current.Col)
+		if !splitters[nextPosition] {
+			current.Row++
+			queue = append(queue, current)
+		} else {
+			left := &BeamRun{
+				Row: nextRow,
+				Col: current.Col - 1,
 			}
-			if run.Col < len(run.Grid[0])-1 {
-				grid := append([]string{}, run.Grid...)
-				row := bytes.Clone([]byte(grid[nextRow]))
-				row[run.Col+1] = '|'
-				grid[nextRow] = string(row)
-				nextRun := BeamRun{Grid: grid, Row: nextRow, Col: run.Col + 1}
-				queue = append(queue, nextRun)
-				split = true
+			right := &BeamRun{
+				Row: nextRow,
+				Col: current.Col + 1,
+			}
+			split := false
+			if left.Col >= 0 && left.Col < width && left.Row < height {
+				if referenceSplits[left.Key()] != nil {
+					referenceSplits[left.Key()].TimeLineCount++
+				} else {
+					referenceSplits[left.Key()] = left
+					queue = append(queue, left)
+					split = true
+				}
+			}
+			if right.Col >= 0 && right.Col < width && right.Row < height {
+				if referenceSplits[right.Key()] != nil {
+					referenceSplits[right.Key()].TimeLineCount++
+				} else {
+					referenceSplits[right.Key()] = right
+					queue = append(queue, right)
+					split = true
+				}
 			}
 			if split {
-				timelines++
+				timelines += current.TimeLineCount
 			}
 		}
+
 	}
 	fmt.Println("total timelines", timelines)
 }
 
-func printGrid(grid []string) {
-	for _, row := range grid {
-		println(row)
+func printGrid(splitters map[string]bool, height, width int, run BeamRun) {
+	for r := 0; r < height; r++ {
+		for c := 0; c < width; c++ {
+			if splitters[fmt.Sprintf("%d,%d", r, c)] {
+				fmt.Print("^")
+			} else if r == run.Row && c == run.Col {
+				fmt.Print("x")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println()
 	}
+}
+
+func printQueue(queue []BeamRun) {
+	fmt.Print("Queue: ")
+	for _, run := range queue {
+		fmt.Printf("(%d,%d) ", run.Row, run.Col)
+	}
+	fmt.Println()
 }
